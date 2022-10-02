958d027a71c84f5e8f63d247e4618af66ef51a68
Ryo Somatani <ryo_somatani@outlook.com>
2022-10-02 10:35:23 -0700

    reservations - fix validations for conflicts

--- helpers/roomHelpers.js
+++ helpers/roomHelpers.js
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 import moment from 'moment';
 import Room from '../models/room.js';
 import Reservation from '../models/reservation.js';
@@ -18,7 +17,7 @@ const validateReservation = async (reqData) => {
   const validTypes = ['weekly', 'non_weekly'];
   if (!validTypes.includes(type)) {
     return {
-      error: 'wrong type provided',
+      error: 'wrong type provided. use "weekly" or "non_weekly".',
       statusCode: 400,
     };
   }
@@ -47,102 +46,19 @@ const validateReservation = async (reqData) => {
     }
   } catch (e) {
     return {
-      error: 'invalid ID provided',
+      error: 'invalid ID format provided',
       statusCode: 400,
     };
   }
-  // non_weekly reservation
-  if (type === 'non_weekly') {
-    // check for conflicts
-    try {
-      const existingReservations = await Reservation.find({ room_id });
-      if (existingReservations.length) {
-        const dateConflictedReservations = existingReservations.filter(
-          (reservation) => {
-            const endDateToUse =
-              reservation.type === 'non_weekly'
-                ? reservation.end_date
-                : moment(reservation.end_date)
-                    .add(reservation.duration - 1, 'weeks')
-                    .format('YYYY-MM-DD');
-            // check term conflict
-            const isTermConflicted = !(
-              end_date < reservation.start_date ||
-              reservation.start_date > endDateToUse
-            );
-            if (!isTermConflicted) return false;
 
-            // check day of week conflict
-            const allFirstWeekExistingDates = getDatesBetween(
-              reservation.start_date,
-              reservation.end_date
-            );
-            const allRequestedDates = getDatesBetween(start_date, end_date);
-            const allExistingDates = getDatesBetween(
-              reservation.start_date,
-              endDateToUse
-            );
-            // all days of week for each requested and existing dates
-            const requestedDows = [
-              ...new Set(
-                allRequestedDates.map((date) => moment(date).format('dddd'))
-              ),
-            ];
-            const existingDows = [
-              ...new Set(
-                allFirstWeekExistingDates.map((date) =>
-                  moment(date).format('dddd')
-                )
-              ),
-            ];
-
-            // check if conflicted dates are included days of week for each requested and existing dates
-            const truelyConflicted =
-              allRequestedDates.find((requestedDate) => {
-                if (allExistingDates.includes(requestedDate)) {
-                  const dow = moment(requestedDate).format('dddd');
-                  return (
-                    requestedDows.includes(dow) && existingDows.includes(dow)
-                  );
-                }
-                return false;
-              }) != null;
-
-            return truelyConflicted;
-          }
-        );
-        const isConflicted =
-          dateConflictedReservations.find((reservation) =>
-            isTimeConflicted(
-              start_time,
-              end_time,
-              reservation.start_time,
-              reservation.end_time
-            )
-          ) != null;
-        if (isConflicted) {
-          return {
-            error: 'reservation conflict found',
-            statusCode: 400,
-          };
-        }
-      }
-    } catch (e) {
-      console.log(e);
-      return {
-        error: 'unexpected error occured',
-        statusCode: 500,
-      };
-    }
-
-    // weekly reservation
-  } else {
+  if (type === 'weekly') {
     // check if start_date and end_date are in the same week
     const startWeek = moment(start_date).week();
     const endWeek = moment(end_date).week();
     if (startWeek !== endWeek) {
       return {
-        error: 'start_date and end_date need to be in the same week',
+        error:
+          'start_date and end_date need to be in the same week for a "weekly" reservation',
         statusCode: 400,
       };
     }
@@ -150,7 +66,8 @@ const validateReservation = async (reqData) => {
     // check duration value
     if (duration < 1) {
       return {
-        error: 'wrong duration value provided. It needs to be at least 1',
+        error:
+          'wrong duration value provided. It needs to be bigger than or equal to 1',
         statusCode: 400,
       };
     }
@@ -165,108 +82,110 @@ const validateReservation = async (reqData) => {
         statusCode: 400,
       };
     }
+  }
 
-    // check for conflicts
-    try {
-      const existingReservations = await Reservation.find({ room_id });
-      if (existingReservations.length) {
-        const dateConflictedReservations = existingReservations.filter(
-          (reservation) => {
-            const endDateToUse =
-              reservation.type === 'non_weekly'
-                ? moment(reservation.end_date).format('YYYY-MM-DD')
-                : moment(reservation.end_date)
-                    .add(reservation.duration - 1, 'weeks')
-                    .format('YYYY-MM-DD');
-            console.log(
-              'reservation.end_date.format("YYYY-MM-DD")',
-              moment('2022-10-11').format('YYYY-MM-DD')
-            );
-            console.log('reservation.end_date', '2022-10-11T00:00:00.000Z');
+  // check for conflicts
+  try {
+    const existingReservations = await Reservation.find({ room_id });
+    if (existingReservations.length) {
+      const dateConflictedReservations = existingReservations.filter(
+        (reservation) => {
+          const requestEndDateToUse =
+            type === 'non_weekly'
+              ? moment(end_date).format('YYYY-MM-DD')
+              : moment(end_date)
+                  .add(duration - 1, 'weeks')
+                  .format('YYYY-MM-DD');
+          const existingEndDateToUse =
+            reservation.type === 'non_weekly'
+              ? moment(reservation.end_date).format('YYYY-MM-DD')
+              : moment(reservation.end_date)
+                  .add(reservation.duration - 1, 'weeks')
+                  .format('YYYY-MM-DD');
 
-            // check term conflict
-            const isTermConflicted = !(
-              moment(end_date)
-                .add(duration - 1, 'weeks')
-                .format('YYYY-MM-DD') <
-                moment(reservation.start_date).format('YYYY-MM-DD') ||
-              moment(reservation.start_date).format('YYYY-MM-DD') > endDateToUse
-            );
-            if (!isTermConflicted) return false;
+          // check term conflict
+          const isTermConflicted = !(
+            requestEndDateToUse <
+              moment(reservation.start_date).format('YYYY-MM-DD') ||
+            moment(reservation.start_date).format('YYYY-MM-DD') >
+              existingEndDateToUse
+          );
+          if (!isTermConflicted) return false;
 
-            // check day of week conflict
-            const allFirstWeekRequestedDates = getDatesBetween(
-              start_date,
-              moment(end_date).format('YYYY-MM-DD')
-            );
-            const allFirstWeekExistingDates = getDatesBetween(
-              reservation.start_date,
-              moment(reservation.end_date).format('YYYY-MM-DD')
-            );
-            const allRequestedDates = getDatesBetween(
-              start_date,
-              moment(end_date)
-                .add(duration - 1, 'weeks')
-                .format('YYYY-MM-DD')
-            );
-            const allExistingDates = getDatesBetween(
-              reservation.start_date,
-              endDateToUse
-            );
-            // all days of week for each requested and existing dates
-            const requestedDows = [
-              ...new Set(
-                allFirstWeekRequestedDates.map((date) =>
-                  moment(date).format('dddd')
-                )
-              ),
-            ];
-            const existingDows = [
-              ...new Set(
-                allFirstWeekExistingDates.map((date) =>
-                  moment(date).format('dddd')
-                )
-              ),
-            ];
+          // check day of week conflict
+          const allFirstWeekRequestedDates = getDatesBetween(
+            moment(start_date).format('YYYY-MM-DD'),
+            moment(end_date).format('YYYY-MM-DD')
+          );
+          const allFirstWeekExistingDates = getDatesBetween(
+            moment(reservation.start_date).format('YYYY-MM-DD'),
+            moment(reservation.end_date).format('YYYY-MM-DD')
+          );
+          const allRequestedDates = getDatesBetween(
+            moment(start_date).format('YYYY-MM-DD'),
+            requestEndDateToUse
+          );
+          const allExistingDates = getDatesBetween(
+            moment(reservation.start_date).format('YYYY-MM-DD'),
+            existingEndDateToUse
+          );
+          // all days of week for each requested and existing dates
+          const baseDatesToRetrieveDowFrom =
+            type === 'non_weekly'
+              ? allRequestedDates
+              : allFirstWeekRequestedDates;
+          const requestedDows = [
+            ...new Set(
+              baseDatesToRetrieveDowFrom.map((date) =>
+                moment(date).format('dddd')
+              )
+            ),
+          ];
+          const existingDows = [
+            ...new Set(
+              allFirstWeekExistingDates.map((date) =>
+                moment(date).format('dddd')
+              )
+            ),
+          ];
 
-            // check if conflicted dates are included days of week for each requested and existing dates
-            const truelyConflicted =
-              allRequestedDates.find((requestedDate) => {
-                if (allExistingDates.includes(requestedDate)) {
-                  const dow = moment(requestedDate).format('dddd');
-                  return (
-                    requestedDows.includes(dow) && existingDows.includes(dow)
-                  );
-                }
-                return false;
-              }) != null;
+          // check if conflicted dates are included days of week for each requested and existing dates
+          const truelyConflicted =
+            allRequestedDates.find((requestedDate) => {
+              if (allExistingDates.includes(requestedDate)) {
+                const dow = moment(requestedDate).format('dddd');
+                return (
+                  requestedDows.includes(dow) && existingDows.includes(dow)
+                );
+              }
+              return false;
+            }) != null;
 
-            return truelyConflicted;
-          }
-        );
-        const isConflicted =
-          dateConflictedReservations.find((reservation) =>
-            isTimeConflicted(
-              start_time,
-              end_time,
-              reservation.start_time,
-              reservation.end_time
-            )
-          ) != null;
-        if (isConflicted) {
-          return {
-            error: 'reservation conflict found',
-            statusCode: 400,
-          };
+          return truelyConflicted;
         }
+      );
+      const isConflicted =
+        dateConflictedReservations.find((reservation) =>
+          isTimeConflicted(
+            start_time,
+            end_time,
+            reservation.start_time,
+            reservation.end_time
+          )
+        ) != null;
+      if (isConflicted) {
+        return {
+          error: 'reservation conflict found with provided date',
+          statusCode: 400,
+        };
       }
-    } catch (e) {
-      console.log(e);
-      return {
-        error: 'unexpected error occured',
-        statusCode: 500,
-      };
     }
+  } catch (e) {
+    console.log(e);
+    return {
+      error: 'unexpected error occured',
+      statusCode: 500,
+    };
   }
 
   return {
@@ -302,65 +221,5 @@ const getDatesBetween = (startDate, endDate) => {
   }
   return dates;
 };
-
-// const isDateAndTimeConflicted = async ({
-//   room_id,
-//   start_date,
-//   end_date,
-//   start_time,
-//   end_time,
-//   duration,
-// }) => {
-//   try {
-//     const existingReservations = await Reservation.find({
-//       room_id: room_id,
-//       $or: [
-//         {
-//           start_date: { $lte: start_date },
-//           end_date: {
-//             $gte: duration
-//               ? moment(start_date).subtract(duration, 'weeks')
-//               : start_date,
-//           },
-//         },
-//         {
-//           start_date: {
-//             $lte: duration ? moment(end_date).add(duration, 'weeks') : end_date,
-//           },
-//           end_date: {
-//             $gte: duration
-//               ? moment(end_date).subtract(duration, 'weeks')
-//               : end_date,
-//           },
-//         },
-//       ],
-//     });
-//     console.log('existingReservations', existingReservations);
-//     const isConflicted =
-//       existingReservations.find((reservation) =>
-//         isTimeConflicted(
-//           start_time,
-//           end_time,
-//           reservation.start_time,
-//           reservation.end_time
-//         )
-//       ) != null;
-
-//     return isConflicted;
-//   } catch (e) {
-//     return {
-//       error: 'unexpected error occured',
-//       statusCode: 500,
-//     };
-//   }
-// };
-
-// const getAllDatesFromStart_date = (start_date, numberOfDays) => {
-//   const dates = [];
-//   for (let i = 0; i < numberOfDays + 1; i++) {
-//     dates.push(moment(start_date).add(i, 'days'));
-//   }
-//   return dates;
-// };
 
 export { validateReservation };
