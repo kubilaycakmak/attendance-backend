49f2232d8f09ffc0205f542b6b5098e49d01fe53
Yuya Satake <sata.yu.7878@gmail.com>
2022-10-06 17:39:06 -0700

    created function to get closest avaliable time for room

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 // const detectConflictedTime = (d1, d2) => {
 //   // if (d1.end >= d2.start && d2.start <= d1.end) return true
 //   if (d1.end >= d2.start && d1.start <= d2.end) return true;
@@ -21,9 +23,80 @@
 
 // console.log(detectConflictedTime(d1.date, d2.date));
 
-const a = [[1, 56]].map((b) => {
-  return b.map((c) => {
-    return c * 2;
+// const a = [[1, 56]].map((b) => {
+//   return b.map((c) => {
+//     return c * 2;
+//   });
+// });
+// console.log(a);
+
+// get next available time
+const nextAvailableTime = () => {
+  const now = moment();
+
+  const format = 'hh:mm';
+  const startTime = moment('08:00', format);
+  const endTime = moment('21:30', format);
+
+  // TODO: get reservation as of now
+  // const reservations = await Reservation.find({ room_id });
+  const reservations = [
+    {
+      roomId: '6335e24c64e8a6df4ed2223f',
+      type: 'weekly',
+      startDate: '2022-10-05',
+      endDate: '2022-10-07',
+      startTime: '11:00',
+      endTime: '12:30',
+      duration: 12,
+    },
+    {
+      roomId: '6335e24c64e8a6df4ed2223f',
+      type: 'weekly',
+      startDate: '2022-10-08',
+      endDate: '2022-10-09',
+      startTime: '15:00',
+      endTime: '17:30',
+      duration: 12,
+    },
+    {
+      roomId: '6335e24c64e8a6df4ed2223f',
+      type: 'weekly',
+      startDate: '2022-10-07',
+      endDate: '2022-10-010',
+      startTime: '15:00',
+      endTime: '17:30',
+      duration: 12,
+    },
+  ];
+
+  //  sort closestReservation to get the newest reservation
+  const sortedReservation = reservations.sort((r1, r2) => {
+    return (
+      moment([r1.endDate, r1.endTime].join('T')) -
+      moment([r2.endDate, r2.endTime].join('T'))
+    );
   });
-});
-console.log(a);
+
+  const closestReservation = sortedReservation[0];
+  const closestStart = moment(
+    [closestReservation.startDate, closestReservation.startTime].join('T')
+  );
+  const closestEnd = moment(
+    [closestReservation.endDate, closestReservation.endTime].join('T')
+  );
+
+  let availableTime;
+  if (now.isBefore(closestStart) && now.isBetween(startTime, endTime)) {
+    availableTime = now;
+  } else {
+    availableTime = closestEnd;
+  }
+
+  return availableTime;
+};
+
+console.log(nextAvailableTime());
+
+// console.log(moment('2022-09-08T12:00').format('MMMM Do YYYY, h:mm:ss a'));
+// console.log(new Date(nextAvailableTime()));
